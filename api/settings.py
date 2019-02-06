44d9b2416eb14690cf4cb1932fef60e59424d7ac
GuillermoCoding <gs8rj@virginia.edu>
2019-02-06 12:47:49 -0500

    scraper app boilerplate

--- api/settings.py
+++ api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'scraper.apps.ScraperConfig'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.DjangoModelPermissionOrAnonReadOnly'
+  ]
+}
